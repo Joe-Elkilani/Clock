f227e3acc346f7c8037e65ce7c83fcf950562afe
Joe-Elkilani <yousefelkilani15112011@gmail.com>
2025-08-11 11:08:28 +0200

    Making Website

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+function updateClock() {
+    const now = new Date();
+    const second = now.getSeconds();
+    const minute = now.getMinutes();
+    const hour = now.getHours();
+
+    const secondDeg = second * 6;
+    const minuteDeg = minute * 6 + second * 0.1;
+    const hourDeg = (hour % 12) * 30 + minute * 0.5;
+
+    document.getElementById('hour').style.transform = `rotate(${hourDeg}deg) translateX(-50%)`;
+    document.getElementById('minute').style.transform = `rotate(${minuteDeg}deg) translateX(-50%)`;
+    document.getElementById('second').style.transform = `rotate(${secondDeg}deg) translateX(-50%)`;
+}
+setInterval(updateClock, 1000);
+updateClock();
